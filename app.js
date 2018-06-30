c7f6c04518cd3bb9ea8387b7abe131bc6ca7a0ae
harrycarriers <harrycarriers@gmail.com>
2018-06-30 18:09:21 +0300

    support english

--- app.js
+++ app.js
@@ -17,12 +17,27 @@ bot.on('message', message => {
 		if (err) console.error(err);
 	})
 	
-	if(msg=== prefix + ('ÇAY') || msg.name=== ":tea:")
+	if(msg === prefix + ('tea'))
 	{
-		var num = Math.floor(Math.random() * 3);
-		if(num == 0) message.channel.send('Çay? <@' + sender.id + '> Çay.');
+		console.log(msg.name);
+		var num = Math.floor(Math.random() * 5);
+		if(num == 0) message.channel.send('I understood you <@' + sender.id + '>, you look hot again.');
+		else if (num == 1) message.channel.send( '<@' + sender.id + '>, you are smelling like a tea!');
+		else if (num == 2) message.channel.send('<@' + sender.id + '>, no more tea to you!');
+		else if (num == 3) message.channel.send('<@' + sender.id + '>, want me to brew a tea for you?');
+		else message.channel.send('<@' + sender.id + '>, could you brew the tea slowly?');
+		
+	}
+	
+	if(msg === prefix + ('ÇAY'))
+	{
+		console.log(msg.name);
+		var num = Math.floor(Math.random() * 5);
+		if(num == 0) message.channel.send('Anlıyorum <@' + sender.id + '>, demlisin yine.');
 		else if (num == 1) message.channel.send( '<@' + sender.id + '>, çay gibi demli kokular alıyorum senden!');
-		else message.channel.send('<@' + sender.id + '>, şakire çay yok!');
+		else if (num == 2) message.channel.send('<@' + sender.id + '>, şakire çay yok!');
+		else if (num == 3) message.channel.send('<@' + sender.id + '>, demliyim mi abi?');
+		else message.channel.send('<@' + sender.id + '>, mümkünse kısık ateşte demler misiniz?');
 		
 	}
 	
@@ -34,7 +49,98 @@ bot.on('message', message => {
 				name : sender.username,
 				icon_url: sender.avatarURL
 			},
-			description: ":tea: Çay Puanınız : " + userData[sender.id + message.guild.id].puan
+			description: 'Çay Puanınız : ' + userData[sender.id + message.guild.id].puan + ':tea:'
+		}});
+	}
+	
+	if(msg=== prefix + 'BONUS'){
+		var d = new Date();
+		if(!userData[sender.id + message.guild.id]) userData[sender.id + message.guild.id] = {};
+		if(!userData[sender.id + message.guild.id].sure || userData[sender.id + message.guild.id].sure.getDay - d.getDay >= 1){
+			userData[sender.id + message.guild.id].puan += 25; 
+			userData[sender.id + message.guild.id].sure = d;
+			message.channel.send({embed: 
+			{
+			color:0x30db52,
+			author : {
+				name : bot.user.username,
+				icon_url: bot.user.avatarURL
+			},
+			fields: [{
+				name:'Günlük bonus',
+				value:'<@' + sender.id + '>, günlük çayınız demlendi (+25).  Kaç şeker alırdınız?'
+			}]
+			}});
+		}
+		else message.channel.send({embed: 
+			{
+				color:0x30db52,
+				author : {
+					name : bot.user.username,
+					icon_url: bot.user.avatarURL
+				},
+				fields: [{
+					name:'Üzgünüm! :sad:',
+					value:'Günde sadece 1 bardak çay ikram edebiliyorum!'
+			}]
+			}});
+	}
+	
+	if(msg=== prefix + 'DAILY'){
+		var d = new Date();
+		if(!userData[sender.id + message.guild.id]) userData[sender.id + message.guild.id] = {};
+		if(!userData[sender.id + message.guild.id].sure || userData[sender.id + message.guild.id].sure.getDay - d.getDay >= 1){
+			userData[sender.id + message.guild.id].puan += 25; 
+			userData[sender.id + message.guild.id].sure = d;
+			message.channel.send({embed: 
+			{
+			color:0x30db52,
+			author : {
+				name : bot.user.username,
+				icon_url: bot.user.avatarURL
+			},
+			fields: [{
+				name:'Daily bonus',
+				value:'<@' + sender.id + '>, brewed your daily tea (+25).  Want some sugar?'
+			}]
+			}});
+		}
+		else message.channel.send({embed: 
+			{
+				color:0x30db52,
+				author : {
+					name : bot.user.username,
+					icon_url: bot.user.avatarURL
+				},
+				fields: [{
+					name:'So sorry!',
+					value:'I am only allowed to give 1 glass of tea everyday! (Blame my programmer)'
+			}]
+			}});
+	}
+	
+	if(msg=== prefix + 'HELP'){
+
+		message.channel.send({embed: 
+		{
+			color:0x30db52,
+			author : {
+				name : bot.user.username,
+				icon_url: bot.user.avatarURL
+			},
+			fields: [{
+				name : '-tea',
+				value : 'Would you like to drink some tea with me?',
+			},
+			{
+				name : '-points',
+				value : 'Show my current tea points.',
+			},
+			{
+				name : '-daily',
+				value : 'Get your daily tea points.',
+			}
+			]
 		}});
 	}
 	
@@ -48,26 +154,23 @@ bot.on('message', message => {
 				icon_url: bot.user.avatarURL
 			},
 			fields: [{
-				name : "-çay",
-				value : "Deneme komutu.",
-				inline : true
+				name : '-çay',
+				value : 'Çay içmek istedi canın değil mi?',
 			},
 			{
-				name : "-puanım",
-				value : "Mevcut çay puanımı göster.",
-				inline : true
+				name : '-puanım',
+				value : 'Mevcut çay puanımı göster.',
 			},
 			{
-				name : "-bonus",
-				value : "Günlük çay puanını almak için yaz. (planlanıyor)",
-				inline : true
+				name : '-bonus',
+				value : 'Günlük çay puanını almak için yaz.',
 			}
 			]
 		}});
 	}
 });
 
-bot.on('ready', () => { bot.user.setActivity('-yardım')});
+bot.on('ready', () => { bot.user.setActivity('-yardım, -help')});
 bot.on('ready', () => { console.log('OchaBot başladı.')});
 
 bot.login('NDYyMzc3MTEyOTg5MzM1NTcy.Dhg92A.GiLaFONey9oKxPHeGhzswQeEnU8');
